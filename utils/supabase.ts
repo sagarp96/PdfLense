52d6258ee63d4a8949739eca486d6eddc2705c3e
sagarp96 <sagarcool2345@gmail.com>
2025-09-15 18:00:44 +0530

    Env issues resolve

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,14 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
-declare global {
-  interface ImportMetaEnv {
-	VITE_SUPABASE_URL: string;
-	VITE_SUPABASE_ANON_KEY: string;
-  }
-}
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
